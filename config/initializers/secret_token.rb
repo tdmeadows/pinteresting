7e5aa03c85ee10226b9c1046c7addca39f3da50c
tdmeadows <timothydmeadows@gmail.com>
2014-10-21 17:35:11 -0400

    bundle install, all ready to push to heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,16 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-<<<<<<< HEAD
-Pinteresting::Application.config.secret_key_base = '8d9779f9437dffc876dd6dfc1d0d14cd5c306363432d93d2d82032f100bd968ea6a7068337cfcf461920e2678ee9811e2f01d9547955f792631a295d452475c5'
-=======
-Pinteresting::Application.config.secret_key_base = '0be99b2d2cadb75da9a4e147308042e52f9ea5cbb29130740397d4d63236f1554aa350f2aab443225cd70687550721e7fa37016f4691f6953fa162b8813342a7'
->>>>>>> f173be3d4c21198e8771100abc1f06c753905237
+Pinteresting::Application.config.secret_key_base = '096ac06d1e95e342f3589d3455af925bf64d328a1afd84380ea98b6ff92a93cd0ce72e7c3968dd3ec1ea59e2a5b65aad02330a877cbc68eeb86ab5e7beb8ad1d'
